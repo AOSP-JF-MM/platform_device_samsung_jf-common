65d50e2ea350a668ece326272a26a5ecd077be50
B--B <dark.schneider.ds@gmail.com>
2015-12-13 18:00:09 +0100

    Revert RIL changes Bad things with these changes and old ril blobs, incoming call stop working the same thing happens with the new-ril branch (https://github.com/AOSP-JF-MM/platform_device_samsung_jflte/tree/aosp-6.0-test-new-ril)and old blobs I can't test other changes with a non-working phone, revert this commit when we switch to the new ril blobs

--- ril/telephony/java/com/android/internal/telephony/jflteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/jflteRIL.java
@@ -216,6 +216,7 @@ public class jflteRIL extends RIL implements CommandsInterface {
     protected Object
     responseCallList(Parcel p) {
         int num;
+        int voiceSettings;
         ArrayList<DriverCall> response;
         DriverCall dc;
 
@@ -236,24 +237,21 @@ public class jflteRIL extends RIL implements CommandsInterface {
             dc.isMpty = (0 != p.readInt());
             dc.isMT = (0 != p.readInt());
             dc.als = p.readInt();
-            dc.isVoice = (0 != p.readInt());
-            if (!isGSM) {
-		    p.readInt();
-		    p.readInt();
-		    p.readString();
+            voiceSettings = p.readInt();
+            dc.isVoice = (0 == voiceSettings) ? false : true;
+            dc.isVoicePrivacy = (0 != p.readInt());
+            if (isGSM) {
+                p.readInt();
+                p.readInt();
+                p.readString();
             }
-	    dc.isVoicePrivacy = (0 != p.readInt());
             dc.number = p.readString();
-            dc.numberPresentation = DriverCall.presentationFromCLIP(p.readInt());
+            int np = p.readInt();
+            dc.numberPresentation = DriverCall.presentationFromCLIP(np);
             dc.name = p.readString();
-	    if (isGSM) {
-		    // Read namePresentation from CLIP
-		    dc.namePresentation = DriverCall.presentationFromCLIP(p.readInt());
-	    } else {
-		    // Read parcel directly
-		    dc.namePresentation = p.readInt();
-	    }
-            if (p.readInt() == 1) {
+            dc.namePresentation = p.readInt();
+            int uusInfoPresent = p.readInt();
+            if (uusInfoPresent == 1) {
                 dc.uusInfo = new UUSInfo();
                 dc.uusInfo.setType(p.readInt());
                 dc.uusInfo.setDcs(p.readInt());
@@ -580,6 +578,38 @@ public class jflteRIL extends RIL implements CommandsInterface {
         if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
 
         send(rr);
+    }
+
+    //this method is used in the search network functionality.
+    // in mobile network setting-> network operators
+    @Override
+    protected Object
+    responseOperatorInfos(Parcel p) {
+        String strings[] = (String [])responseStrings(p);
+        ArrayList<OperatorInfo> ret;
+
+        if (strings.length % mQANElements != 0) {
+            throw new RuntimeException(
+                                       "RIL_REQUEST_QUERY_AVAILABLE_NETWORKS: invalid response. Got "
+                                       + strings.length + " strings, expected multiple of " + mQANElements);
+        }
+
+        ret = new ArrayList<OperatorInfo>(strings.length / mQANElements);
+        Operators init = null;
+        if (strings.length != 0) {
+            init = new Operators();
+        }
+        for (int i = 0 ; i < strings.length ; i += mQANElements) {
+            String temp = init.unOptimizedOperatorReplace(strings[i+0]);
+            ret.add (
+                     new OperatorInfo(
+                                      temp, //operatorAlphaLong
+                                      temp,//operatorAlphaShort
+                                      strings[i+2],//operatorNumeric
+                                      strings[i+3]));//state
+        }
+
+        return ret;
     }
 
     @Override
